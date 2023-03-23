80246344e9fcda5ba48faa6a3d6c2132d7568992
wanderer <wanderer@gmail.com>
2023-03-23 21:51:57 +0800

    feat: 简单的ssh

--- HoneyBee.Git.Gui/App/View/SSHView.cs
+++ HoneyBee.Git.Gui/App/View/SSHView.cs
@@ -22,9 +22,10 @@ namespace Wanderer.App.View
         private const int m_textMaxLength = 1024 * 100;
 
         private SshClient m_sshClient;
+        private StringBuilder m_stringBuilder = new StringBuilder();
         public SSHView(IContext context) : base(context)
         {
-            m_sshClient = new SshClient("netlighting.cn","root","Zhang123.");
+            m_sshClient = new SshClient("localhost","root","root");
             m_sshClient.HostKeyReceived += (sender, e) =>
             {
                 e.CanTrust = true;
@@ -81,20 +82,44 @@ namespace Wanderer.App.View
 
             //ImGui.PopStyleColor(); 
             //ImGuiInputTextFlags.CallbackEdit |
-            if (ImGui.InputTextMultiline("#OnSShTerminalDraw-InputTextMultiline", ref m_inputText, m_textMaxLength, ImGui.GetWindowSize(), ImGuiInputTextFlags.EnterReturnsTrue| ImGuiInputTextFlags.CallbackCompletion, ImGuiInputTextCallback))
+            m_inputText = m_stringBuilder.ToString();
+
+            if (ImGui.InputTextMultiline("#OnSShTerminalDraw-InputTextMultiline", ref m_inputText, m_textMaxLength, ImGui.GetWindowSize()))
             {
             
-                Log.Info(m_inputText);
+                if (ImGui.IsKeyDown(ImGuiKey.Enter))
+                {
+                    Log.Info(m_inputText);
+                    int index = m_inputText.LastIndexOf("\n",1) + 1;
+                    if (index >= 0& index< m_inputText.Length)
+                    {
+                        string command = m_inputText.Substring(index, m_inputText.Length - index - 1);
+                        if (!string.IsNullOrEmpty(command))
+                        {
+                            m_stringBuilder.AppendLine(command);
 
+                            if (m_sshClient != null && m_sshClient.IsConnected)
+                            {
+                                using (var cmd = m_sshClient.RunCommand(command))
+                                {
+                                    Log.Info("cmd:{0} ExitStatus:{1} Result:{2}", cmd.CommandText, cmd.ExitStatus, cmd.Result);
+                                    if (!string.IsNullOrEmpty(cmd.Result))
+                                    {
+                                        m_stringBuilder.AppendLine(cmd.Result);
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                }
             }
         }
 
 
         private unsafe int ImGuiInputTextCallback(ImGuiInputTextCallbackData* data)
         {
-            Log.Info(m_inputText);
-            int index = m_inputText.LastIndexOf("\n") + 1;
-            string command = m_inputText.Substring(index, m_inputText.Length - index);
+            
 
             return 0;
         }
