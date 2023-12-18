8561da4b9dc6f5fec0c2ba93bc4df30390f9a080
zhangyang <zhangyang@ghgame.cn>
2023-12-18 10:48:46 +0800

    feat: 更新xmake package

--- imgui_impl_sdl_opengl3/xmake.lua
+++ imgui_impl_sdl_opengl3/xmake.lua
@@ -1,7 +1,9 @@
-add_requires("conan::sdl/2.26.0",{alias="sdl"})
-add_requires("conan::imgui/cci.20220621+1.88.docking",{alias="imgui"})
-add_requires("conan::glew/2.2.0",{alias="glew"})
-add_requires("conan::opengl/system",{alias="opengl"})
+--conan::sdl/2.26.0
+add_requires("libsdl",{alias="sdl"})
+add_requires("imgui v1.88-docking",{alias="imgui"})
+add_requires("glew",{alias="glew"})
+add_requires("opengl",{alias="opengl"})
+add_requires("libgit2 v0.26.2",{alias="libgit2"})
 -- add_requires("stb")
 -- add_requires("luajit")
 
@@ -11,14 +13,14 @@ target("iiso3")
     -- add_defines("LUA_BUILD_AS_DLL","LUA_LIB")
     add_files("src/*.cpp")
     -- add_includedirs("imgui/include")
-    add_packages("sdl","glew","opengl","imgui")
+    add_packages("sdl","glew","opengl","imgui","libgit2")
 
 target("test")
     set_kind("binary")
     set_languages("cxx17")
     add_files("test/main.cpp")
     add_includedirs("src")
-    add_packages("sdl","glew","opengl","imgui")
+    add_packages("sdl","glew","opengl","imgui","libgit2")
     add_deps("iiso3")
 
 -- 生成clion可识别的 compile_commands
